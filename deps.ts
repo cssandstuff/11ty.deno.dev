3843cf7b29257aea088e91fb3d347e50a55892c9
Tim Clulow <tim@cssandstuff.com>
2024-03-26 21:20:35 +1300

    file organisation

--- deps.ts
+++
@@ -1,5 +0,0 @@
-export { Application, Context, Router, Response, Status, send } from "https://deno.land/x/oak@14.2.0/mod.ts";
-export { staticFileMiddleware } from "./staticFileMiddleware.ts";
-export { viewEngine, etaEngine, oakAdapter }
-  from "https://deno.land/x/view_engine@v10.6.0/mod.ts"
-
